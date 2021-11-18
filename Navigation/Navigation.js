780ad1e00be24ef8b382291415107f641032d1c0
sakout-reda <sakout-reda@hotmail.fr>
2021-11-18 23:37:54 +0100

    commit

---
+++ Navigation/Navigation.js
@@ -0,0 +1,74 @@
+import { createStackNavigator} from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator  } from 'react-navigation-tabs'
+import React from 'react' 
+import { StyleSheet, Image } from 'react-native';
+import Search from '../Components/Search'
+import FilmDetail from '../Components/FilmDetail'
+import Favorites from '../Components/Favorites'
+
+const SearchStackNavigator = createStackNavigator({
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      title: 'Rechercher '
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+})
+
+const FavoritesStackNavigator = createStackNavigator({
+  Favorites: {
+    screen: Favorites,
+    navigationOptions: {
+      title: 'Favoris'
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+})
+
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/ic_search.png')}
+            style={styles.icon}/>
+        }
+      }
+    },
+    Favorites: {
+      screen: FavoritesStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/ic_favorite.png')}
+            style={styles.icon}/>
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD',
+      inactiveBackgroundColor: '#FFFFFF',
+      showLabel: false,
+      showIcon: true
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)
